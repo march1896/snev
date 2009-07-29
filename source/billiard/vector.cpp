cfa32b786a2cbbaf4823e3f12ee0fcf8a0ecebb4
March1896 <March1896@3189ae6c-2c87-11de-a35f-41c58be005ae>
2009-07-29 11:52:15 +0000

    add protection to vector class

--- source/billiard/vector.cpp
+++ source/billiard/vector.cpp
@@ -3,43 +3,51 @@
 
 vector2::vector2(): x( 0.0 ), y( 0.0 ) {}
 vector2::vector2( float _x, float _y ): x( _x ), y( _y )  {}
-vector2::vector2( const vector2 &rhs ): x( rhs.x ), y( rhs.y ) {}
+vector2::vector2( const vector2 &rhs ): x( rhs.getX() ), y( rhs.getY() ) {}
 vector2::~vector2(){}
 
 void vector2::setXY( float _x, float _y ){ x = _x, y = _y; }
 void vector2::setX( float _x ) { x = _x;	}
 void vector2::setY( float _y ) { y = _y;	}
-float vector2::getX() {		return x; 	}
-float vector2::getY() {		return y; 	}
+float vector2::getX()const {		return x; 	}
+float vector2::getY()const {		return y; 	}
 
 vector2 vector2::operator=( const vector2 &rhs ) {
-	this->x = rhs.x;
-	this->y = rhs.y;
+	this->x = rhs.getX();
+	this->y = rhs.getY();
+	return *this;
+}
+
+vector2 vector2::operator+=( const vector2 &rhs ) {
+	this->x += rhs.getX();
+	this->y += rhs.getY();
+	return *this;
+}
+
+vector2 vector2::operator-=( const vector2 &rhs ) {
+	this->x -= rhs.getX();
+	this->y -= rhs.getY();
 	return *this;
 }
 
 vector2 vector2::operator+( const vector2 &rhs ) {
-	this->x += rhs.x;
-	this->y += rhs.y;
-	return *this;
+	return vector2( this->x + rhs.getX(), this->y + rhs.getY() );
 }
 
 vector2 vector2::operator-( const vector2 &rhs ) {
-	this->x -= rhs.x;
-	this->y -= rhs.y;
-	return *this;
+	return vector2( this->x - rhs.getX(), this->y - rhs.getY() );
 }
 
 bool vector2::operator==( const vector2 &rhs ) {
-	return ( this->x == rhs.x && this->y == rhs.y );
+	return ( this->x == rhs.getX() && this->y == rhs.getY() );
 }
 
 float vector2::dot( const vector2 &rhs ) {
-	return this->x * rhs.x + this->y * rhs.y;
+	return this->x * rhs.getX() + this->y * rhs.getY();
 }
 
 float vector2::cross( const vector2 &rhs ) {
-	return this->x * rhs.y - this->y * rhs.x;
+	return this->x * rhs.getY() - this->y * rhs.getX();
 }
 
 float vector2::length() {
@@ -55,7 +63,7 @@ vector2 vector2::normalize() {
 
 vector4::vector4(): x( 0.0 ), y( 0.0 ), z( 0.0 ), w( 0.0 ) {}
 vector4::vector4( float _x, float _y, float _z, float _w ): x( _x ), y( _y ), z( _z ), w( _w ) {}
-vector4::vector4( const vector4 &rhs ): x( rhs.x ), y( rhs.y ), z( rhs.z ), w( rhs.w ) {}
+vector4::vector4( const vector4 &rhs ): x( rhs.getX() ), y( rhs.getY() ), z( rhs.getZ() ), w( rhs.getW() ) {}
 vector4::~vector4(){}
 
 void vector4::setXYZ( float _x, float _y, float _z ){ 			x = _x, y = _y, z = _z; 		}
@@ -65,39 +73,47 @@ void vector4::setX( float _x ) { x = _x;	}
 void vector4::setY( float _y ) { y = _y;	}
 void vector4::setZ( float _z ) { z = _z;	}
 void vector4::setW( float _w ) { w = _w;	}
-float vector4::getX() {		return x; 	}
-float vector4::getY() {		return y; 	}
-float vector4::getZ() {		return z; 	}
-float vector4::getW() {		return w; 	}
+float vector4::getX()const {		return x; 	}
+float vector4::getY()const {		return y; 	}
+float vector4::getZ()const {		return z; 	}
+float vector4::getW()const {		return w; 	}
 
 vector4 vector4::operator=( const vector4 &rhs ) {
-	this->x = rhs.x;
-	this->y = rhs.y;
-	this->z = rhs.z;
-	this->w = rhs.w;
+	this->x = rhs.getX();
+	this->y = rhs.getY();
+	this->z = rhs.getZ();
+	this->w = rhs.getW();
+	return *this;
+}
+
+vector4 vector4::operator+=( const vector4 &rhs ) {
+	this->x += rhs.getX();
+	this->y += rhs.getY();
+	this->z += rhs.getZ();
+	return *this;
+}
+
+vector4 vector4::operator-=( const vector4 &rhs ) {
+	this->x -= rhs.getX();
+	this->y -= rhs.getY();
+	this->z -= rhs.getZ();
 	return *this;
 }
 
 vector4 vector4::operator+( const vector4 &rhs ) {
-	this->x += rhs.x;
-	this->y += rhs.y;
-	this->z += rhs.z;
-	return *this;
+	return vector4( this->x + rhs.getX(), this->y + rhs.getY(), this->z + rhs.getZ(), this->w );
 }
 
 vector4 vector4::operator-( const vector4 &rhs ) {
-	this->x -= rhs.x;
-	this->y -= rhs.y;
-	this->z -= rhs.z;
-	return *this;
+	return vector4( this->x - rhs.getX(), this->y - rhs.getY(), this->z - rhs.getZ(), this->w );
 }
 
 bool vector4::operator==( const vector4 &rhs ) {
-	return ( this->x == rhs.x && this->y == rhs.y && this->z == rhs.z && this->w == rhs.w );
+	return ( this->x == rhs.getX() && this->y == rhs.getY() && this->z == rhs.getZ() && this->w == rhs.getW() );
 }
 
 float vector4::dot( const vector4 &rhs ) {
-	return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z + this->w;
+	return this->x * rhs.getX() + this->y * rhs.getY() + this->z * rhs.getZ() + this->w;
 }
 
 vector4 vector4::cross( const vector4 &rhs ) {
@@ -120,51 +136,59 @@ vector4 vector4::normalize() {
 
 vector3::vector3(): x( 0.0 ), y( 0.0 ), z( 0.0 ) {}
 vector3::vector3( float _x, float _y, float _z ): x( _x ), y( _y ), z( _z )  {}
-vector3::vector3( const vector3 &rhs ): x( rhs.x ), y( rhs.y ), z( rhs.z ) {}
+vector3::vector3( const vector3 &rhs ): x( rhs.getX() ), y( rhs.getY() ), z( rhs.getZ() ) {}
 vector3::~vector3(){}
 
 void vector3::setXYZ( float _x, float _y, float _z ){ x = _x, y = _y, z = _z; }
 void vector3::setX( float _x ) { x = _x;	}
 void vector3::setY( float _y ) { y = _y;	}
 void vector3::setZ( float _z ) { z = _z;	}
-float vector3::getX() {		return x; 	}
-float vector3::getY() {		return y; 	}
-float vector3::getZ() {		return z; 	}
+float vector3::getX()const {		return x; 	}
+float vector3::getY()const {		return y; 	}
+float vector3::getZ()const {		return z; 	}
 
 vector3 vector3::operator=( const vector3 &rhs ) {
-	this->x = rhs.x;
-	this->y = rhs.y;
-	this->z = rhs.z;
+	this->x = rhs.getX();
+	this->y = rhs.getY();
+	this->z = rhs.getZ();
+	return *this;
+}
+
+vector3 vector3::operator+=( const vector3 &rhs ) {
+	this->x += rhs.getX();
+	this->y += rhs.getY();
+	this->z += rhs.getZ();
+	return *this;
+}
+
+vector3 vector3::operator-=( const vector3 &rhs ) {
+	this->x -= rhs.getX();
+	this->y -= rhs.getY();
+	this->z -= rhs.getZ();
 	return *this;
 }
 
 vector3 vector3::operator+( const vector3 &rhs ) {
-	this->x += rhs.x;
-	this->y += rhs.y;
-	this->z += rhs.z;
-	return *this;
+	return vector3( this->x + rhs.getX(), this->y + rhs.getY(), this->z + rhs.getZ() );
 }
 
 vector3 vector3::operator-( const vector3 &rhs ) {
-	this->x -= rhs.x;
-	this->y -= rhs.y;
-	this->z -= rhs.z;
-	return *this;
+	return vector3( this->x - rhs.getX(), this->y - rhs.getY(), this->z - rhs.getZ() );
 }
 
 bool vector3::operator==( const vector3 &rhs ) {
-	return ( this->x == rhs.x && this->y == rhs.y && this->z == rhs.z );
+	return ( this->x == rhs.getX() && this->y == rhs.getY() && this->z == rhs.getZ() );
 }
 
 float vector3::dot( const vector3 &rhs ) {
-	return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+	return this->x * rhs.getX() + this->y * rhs.getY() + this->z * rhs.getZ();
 }
 
 vector3 vector3::cross( const vector3 &rhs ) {
 	vector3 ret;
-	ret.x = this->y * rhs.z - this->z * rhs.y;
-	ret.y = this->x * rhs.z - this->z * rhs.x;
-	ret.z = this->x * rhs.y - this->y * rhs.x;
+	ret.setX( this->y * rhs.getZ() - this->z * rhs.getY() );
+	ret.setY( this->x * rhs.getZ() - this->z * rhs.getX() );
+	ret.setZ( this->x * rhs.getY() - this->y * rhs.getX() );
 	return ret;
 }
 
