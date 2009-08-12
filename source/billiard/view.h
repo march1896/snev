b0ae0541fdcb0766266cd93d762367e5eb2e9957
March1896 <March1896@3189ae6c-2c87-11de-a35f-41c58be005ae>
2009-08-12 07:22:16 +0000

    add display module, it is the first edition, just copy from nehe sdk

---
+++ source/billiard/view.h
@@ -0,0 +1,79 @@
+/*	Nehe SDK
+	Copyright 2002 Jeff Molofee, Gregory Austwick and Others
+
+	Title:
+		View Matrix proxy	
+
+	Version History:
+		v1.00	New class	(GA 11/11/02)
+
+	Licence:
+
+	This SDK/library is free software, written primarily for
+	teaching purposes. You can redistribute and modify it to 
+	your heart's content. However, any use of this library is 
+	at the user's own risk. There is no warranty. The authors 
+	will not be held responsible for ANYTHING that happens as 
+	a result of the use of this software. All financial/emotional/health 
+	problems or anything else that happens after using this 
+	software is therefore the user's own responsibility. 
+	Nevertheless, if the user finds it useful, an email or a 
+	mention in any credits would be appreciated, but is not 
+	necessary.
+*/
+
+#ifndef VIEW_H
+#define VIEW_H
+
+#include "vector.h"
+#include <gl\gl.h>
+#include <gl\glu.h>
+
+/* the 'include' files in opengl.h, which is defined in nehe sdk
+#include <windows.h>    // Header File For Windows
+#include <gl\gl.h>		// Header File For The OpenGL32 Library
+#include <gl\glu.h>		// Header File For The GLu32 Library
+#include "win32.h"		// the windows class
+#include "state.h"		// the state manager	
+#include "timer.h"		// Timing functions
+
+#ifdef _MSC_VER
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+*/
+
+//#pragma comment(lib,"opengl32")
+//#pragma comment(lib,"glu32")
+
+class View {
+public:
+	//
+	//	Geometric functions
+	//
+	// reset the view
+	void Reset();
+	// translate view by (x,y,z)
+	void Translate(float x,float y,float z);
+	void Translate(vector3 vec)			{ this->Translate(vec.getX(),vec.getY(),vec.getZ()); };
+	// axis-angle rotation
+	void Rotate(float angle,float x,float y,float z);
+	void Rotate(float angle,vector3 vec) { this->Rotate(angle,vec.getX(),vec.getY(),vec.getZ()); };
+	// scaling by (x,y,z)
+	void Scale(float x,float y,float z);
+	void Scale(vector3 vec)				{ this->Scale(vec.getX(),vec.getY(),vec.getX()); };
+	// look at function
+	void LookAt(vector3 pos,vector3 at,vector3 up);
+	// save the view
+	void Save();
+	// restore the view
+	void Restore();
+
+	//
+	//	2D functions
+	//
+	// move to position in 2d
+	void Pos2D(int x,int y);
+	void Pos2D(float x,float y);
+};
+
+#endif
