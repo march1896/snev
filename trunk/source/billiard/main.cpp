a43a1b4efcb95e60f70e7f251b2f740a192d4df2
March1896 <March1896@3189ae6c-2c87-11de-a35f-41c58be005ae>
2009-09-04 03:39:01 +0000

    add *.game to implement easier game for shader test

---
+++ trunk/source/billiard/main.cpp
@@ -0,0 +1,75 @@
+#include "gl\glew.h"
+#include "timer.h"
+
+#include "ball.h"
+#include "vector.h"
+#include "collision.h"
+#include "table.h"
+#include "win32.h"
+#include "opengl.h"
+#include "log.h"
+#include "color.h"
+#include "render.h"
+#include "view.h"
+#include "light.h"
+#include "control.h"
+#include "shadow.h"
+#include "zmath.h"
+#include "shader.h"
+#include <cmath>
+#include <cstdio>
+
+bool (*_GameMainLoop_)( float deltatime );
+typedef void (*INITFUNC)();
+INITFUNC _GameInit_, _GameDeinit_;
+
+const float DELTATIME = 33.0 / 1000.0;
+
+#include "gameloop1.game"
+
+void Init() {
+	// we must init _GameMainLoop_(), _GameInit_(), _GameDeinit_() here;
+	_GameMainLoop_ = gameloop1;
+	_GameInit_ = gameinit1;
+	_GameDeinit_ = gamedeinit1;
+}
+
+
+int main() {
+	bool Running = true;
+	float deltatime = DELTATIME;
+	MSG 	msg;
+
+	glewInit();
+	Frame GameFrame;
+	Init();
+	
+	_GameInit_();
+
+	while ( Running )
+	{
+		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))	// Is There A Message Waiting?
+		{
+			if (msg.message==WM_QUIT)				// Have We Received A Quit Message?
+			{
+				Running = false;							// If So done=TRUE
+			}
+			else									// If Not, Deal With Window Messages
+			{
+				TranslateMessage(&msg);				// Translate The Message
+				DispatchMessage(&msg);				// Dispatch The Message
+			}
+		}
+
+		deltatime = GameFrame.GetLength();
+		_GameMainLoop_( DELTATIME );
+
+		if ( deltatime < DELTATIME ) {
+			Sleep( static_cast<DWORD> ( DELTATIME - deltatime) );
+		}
+	}
+
+	_GameDeinit_();
+	return 0;
+}
+
