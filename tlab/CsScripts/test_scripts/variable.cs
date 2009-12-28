02ba5610fbee3f9f277a319ea58bba78105c966a
March1896 <March1896@3189ae6c-2c87-11de-a35f-41c58be005ae>
2009-12-28 11:07:45 +0000

    bring it home

--- tlab/CsScripts/test_scripts/variable.cs
+++ tlab/CsScripts/test_scripts/variable.cs
@@ -2,8 +2,13 @@
 	$str = "Hello_"
 	$str = $str + "Tangel"
 	$str = "Fuck_" + $str
-
 	&print( $str )
+
+	$str = -3 * ( 8 * ( 10 - 2 * 4 ) )
+	&print( $str )
+
+	$str = $var = 0.5 / ( 5 + 10 / 10 - 1 )
+	&print( $var, $str )
 }
 
 
