6fa59bb7b69c6c95e85d17ec7620cf0f9f1bfc9d
March1896 <March1896@3189ae6c-2c87-11de-a35f-41c58be005ae>
2010-03-03 09:27:06 +0000

    add msdt

---
+++ tlab/msdt/sdt_atom.cpp
@@ -0,0 +1,162 @@
+#include "sdt_def.h"
+#include <cstdlib>
+
+static u32 mantissatable[ 2048 ];
+static u32 exponenttable[ 64 ];
+static u32 offsettable[ 64 ];
+static u32 basetable[ 1024 ];
+static u32 shifttable[ 512 ];
+
+static u32 convertmantissa(u32 i);
+static void generatetables();
+
+void hlaf_float_init() {
+	mantissatable[ 0 ]  = 0;
+	for ( int i = 1; i < 1024; i ++ ) {
+		mantissatable[i] = convertmantissa( i );
+	}
+	for ( int i = 1024; i < 2048; i ++ ) {
+		mantissatable[i] = 0x38000000 + ((i-1024)<<13);
+	}
+
+	exponenttable[0] = 0;
+	for ( int i = 1; i < 30; i ++ ) {
+		exponenttable[i] = i<<23;
+	}
+	exponenttable[31]= 0x47800000;
+	exponenttable[32]= 0x80000000;
+	for ( int i = 33; i < 63; i ++ ) {
+		exponenttable[i] = 0x80000000 + (i-32)<<23;
+	}
+	exponenttable[63]= 0xC7800000;
+
+	for ( int i = 0; i < 64; i ++ ) {
+		offsettable[i] = 1024;
+	}
+
+	offsettable[0] = 0;
+	offsettable[32]= 0;
+
+	generatetables();
+}
+
+void generatetables(){
+	u32 i;
+	int e;
+	for(i=0; i<256; ++i){
+		e=i-127;
+		if(e<-24){ // Very small numbers map to zero
+			basetable[i|0x000]=0x0000;
+			basetable[i|0x100]=0x8000;
+			shifttable[i|0x000]=24;
+			shifttable[i|0x100]=24;
+		}
+		else if(e<-14){ // Small numbers map to denorms
+			basetable[i|0x000]=(0x0400>>(18-e));
+			basetable[i|0x100]=(0x0400>>(18-e)) | 0x8000;
+			shifttable[i|0x000]=-e-1;
+			shifttable[i|0x100]=-e-1;
+		}
+		else if(e<=15){ // Normal numbers just lose precision
+			basetable[i|0x000]=((e+15)<<10);
+			basetable[i|0x100]=((e+15)<<10) | 0x8000;
+			shifttable[i|0x000]=13;
+			shifttable[i|0x100]=13;
+		}
+		else if(e<128){ // Large numbers map to Infinity
+			basetable[i|0x000]=0x7C00;
+			basetable[i|0x100]=0xFC00;
+			shifttable[i|0x000]=24;
+			shifttable[i|0x100]=24;
+		}
+		else{ // Infinity and NaN's stay Infinity and NaN's
+			basetable[i|0x000]=0x7C00;
+			basetable[i|0x100]=0xFC00;
+			shifttable[i|0x000]=13;
+			shifttable[i|0x100]=13;
+		}
+	}
+}
+
+u32 convertmantissa(u32 i){
+	u32 m=i<<13; // Zero pad mantissa bits
+	u32 e=0; // Zero exponent
+	while(!(m&0x00800000)){ // While not normalized
+		e-=0x00800000; // Decrement exponent (1<<23)
+		m<<=1; // Shift mantissa
+	}
+	m&=~0x00800000; // Clear leading 1 bit
+	e+=0x38800000; // Adjust bias ((127-14)<<23)
+	return m | e; // Return combined number
+}
+
+f32 slope2float( sdt_slope h ) {
+	//return f32( in ) / 1000.0f;
+	u32 ff = mantissatable[offsettable[h>>10]+(h&0x3ff)]+exponenttable[h>>10] | ((h&0x8000)<<16);
+	//u32 ff = ((h&0x8000)<<16) | (((h&0x7c00)+0x1C000)<<13) | ((h&0x03FF)<<13);
+	f32 f = *((f32*)(&ff));
+	return f;
+}
+
+sdt_slope float2slope( f32 ff ) {
+	//assert( in < 32.0f && in > -32.0f );
+	u32 f = *((u32*)(&ff));
+
+	sdt_slope s = basetable[(f>>23)&0x1ff]+((f&0x007fffff)>>shifttable[(f>>23)&0x1ff]) | ((f>>16)&0x8000);
+	return s;
+}
+
+sdt_data sdt_frame_get( sdt_frame* pf, sdt_length l ) {
+	f32 x = slope2float( pf->slope );
+	return pf->start + l * x;
+}
+
+void sdt_frame_unit_init( sdt_frame_unit* pfu ) {
+	if ( pfu == NULL ) return;
+	pfu->next = NULL;
+	pfu->start = 0;
+	pfu->end = 0;
+	pfu->n_data = 0;
+}
+
+// below is the implementation of sdt_f16
+
+// below is the implementation of frame
+/*
+   sdt_header frame_get_header( sdt_frame* f ) {
+   return *( (sdt_header*)f );
+   }
+
+   sdt_length frame_get_length( sdt_frame* f ) {
+   return *( ((sdt_header*)f) + 1 );
+   }
+
+   sdt_f16 frame_get_slope( sdt_frame* f ) {
+   return *( ((sdt_f16*)f) + 1 );
+   }	
+
+   sdt_f32 frame_get_start( sdt_frame* f ) {
+   return *( ((sdt_f32*)f) + 1 );
+   }
+
+   void frame_set_header( sdt_frame* f, sdt_header h ) {
+   sdt_header* addr = (sdt_header*)f;
+ *addr = h;
+ }
+
+ void frame_set_length( sdt_frame* f, sdt_length l ) {
+ sdt_length* addr = ((sdt_header*)f) + 1;
+ *addr = l;
+ }
+
+ void frame_set_slope( sdt_frame* f, sdt_f16 slope ) {
+ sdt_f16* addr = ((sdt_f16*)f) + 1;
+ *addr = slope;
+ }	
+
+ void frame_set_start( sdt_frame* f, sdt_f32 start ) {
+ sdt_f32* addr = ((sdt_f32*)f) + 1;
+ *addr = start;
+ }
+ */
+
---
+++ tlab/msdt/sdt_pipe.cpp
@@ -0,0 +1,170 @@
+#include "sdt_pipe.h"
+#include "sdt_memory.h"
+
+SDT_Pipe::SDT_Pipe( f32 error, SDT_Memory* pmem ) :
+	curr_upper_slope( 0.0f ),
+	curr_lower_slope( 0.0f ),
+	curr_start( 0.0f ),
+	curr_length( 0 ),
+	max_error( error ),
+	n_data( 0 ),
+	n_memory( 0 ),
+	p_memory( pmem ),
+	p_frame_unit_first( NULL ),
+	p_frame_unit_curr( NULL )
+{
+}
+
+SDT_Pipe::~SDT_Pipe() {
+	// nothing
+}
+
+bool SDT_Pipe::PushBack( sdt_data data ) {
+	if ( curr_length == 0 ) {
+		curr_start = data;
+		curr_upper_slope = MIN_SLOPE;
+		curr_lower_slope = MAX_SLOPE;
+
+		curr_length ++;
+	}
+	else {
+		if ( curr_length + 1 == MAX_LENGTH ) {
+			// the length extend the max length
+			Flush();
+			PushBack( data );
+			return true;
+		}
+
+		f32 diff = data - curr_start;
+		if ( diff / curr_length < MIN_SLOPE || diff / curr_length > MAX_SLOPE ) {
+			Flush();
+			PushBack( data );
+			return true;
+		}
+
+		f32 up, down;
+		up = ( data - curr_start - max_error ) / curr_length;
+		down = ( data - curr_start + max_error ) / curr_length;
+
+		if ( up > curr_lower_slope || down < curr_upper_slope ) {
+			Flush();
+			PushBack( data );
+			return true;
+		}
+
+		if ( up > curr_upper_slope ) curr_upper_slope = up;
+		if ( down < curr_lower_slope ) curr_lower_slope = down;
+		curr_length ++;
+	}
+	return true;
+}
+
+void SDT_Pipe::Flush() {
+	sdt_frame* pfr = GetNewFrame();
+	pfr->start = curr_start;
+	pfr->slope = float2slope( (curr_upper_slope + curr_lower_slope ) / 2 );
+	pfr->length = curr_length;
+
+	p_frame_unit_curr->n_data += curr_length;
+	n_data += curr_length;
+
+	curr_length = 0;
+}
+
+bool SDT_Pipe::PopFront() {
+	// TODO: there no need for the current project
+	return 0;
+}
+
+void SDT_Pipe::PopFirstUnit() {
+	n_data -= p_frame_unit_first->n_data;
+	sdt_frame_unit* to_free = p_frame_unit_first;
+	p_frame_unit_first = p_frame_unit_first->next;
+
+	p_memory->Dealloc( (void*)to_free );
+	n_memory--;
+}
+
+int SDT_Pipe::GetFirstUnitLength() const {
+	return p_frame_unit_first->n_data;
+}
+
+int SDT_Pipe::GetLength() const {
+	return n_data;
+}
+
+int SDT_Pipe::GetMemorySize() const {
+	return n_memory;
+}
+
+sdt_data SDT_Pipe::GetData( int index ) {
+	// assert( curr_length == 0 ); // the data is flushed to used
+
+	if ( index > n_data || index < -n_data ) {
+		// error index!!
+		return 0.0f;
+	}
+	if ( index < 0 ) {
+		index += n_data;
+	}
+
+	int accum = 0;
+
+	sdt_frame_unit* pfu = p_frame_unit_first;
+	do {
+		if ( pfu == NULL ) {
+			// not so many data stored
+			return 0.0f;
+		}
+		if ( accum + pfu->n_data > index ) break;
+		accum += pfu->n_data;
+		pfu = pfu->next;
+	} while ( true );
+
+	int s = index - accum;
+	int k = 0;
+
+	while ( s - pfu->frames[k].length >= 0 ) {
+		s -= pfu->frames[k].length;
+		k ++;
+	}
+
+	return sdt_frame_get( &pfu->frames[k], s );
+}
+
+sdt_frame* SDT_Pipe::GetNewFrame() {
+	sdt_frame* ret = NULL;
+
+	if ( p_frame_unit_first == NULL ) {
+		sdt_frame_unit* pfu = p_memory->Alloc();
+		n_memory ++;
+		sdt_frame_unit_init( pfu );
+
+		p_frame_unit_first = p_frame_unit_curr = pfu;
+
+		ret = &(p_frame_unit_curr->frames[ p_frame_unit_curr->end++ ]);
+	}
+	else {
+		if ( p_frame_unit_curr->end == FRAME_PER_UNIT ) {
+			// need a new frame unit
+			sdt_frame_unit* pfu = p_memory->Alloc();
+			n_memory ++;
+			sdt_frame_unit_init( pfu );
+
+			p_frame_unit_curr->next = pfu;
+			p_frame_unit_curr = pfu;
+
+			ret = &(p_frame_unit_curr->frames[ p_frame_unit_curr->end++ ]);
+		}
+		else {
+			ret = &(p_frame_unit_curr->frames[ p_frame_unit_curr->end ++ ] );
+		}
+	}
+
+	return ret;
+}
+
+void SDT_Pipe::SetMemoryCallback( SDT_Memory* pmem ) {
+	p_memory = pmem;
+}
+
