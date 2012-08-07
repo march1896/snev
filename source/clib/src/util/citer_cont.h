9b3fd9c54a9c1db9cb1f0ac5ecae5b4096a19833
march1896 <March1896@gmail.com>
2012-08-07 01:50:13 +0000

    complete partial of merge sorting, for link-list iterators, there seems some other character then random access iterator, just like if you remove an reference in the container that the iterator holds, it will not impact other elements of the container, but it's need a better abstraction.

--- source/clib/src/util/citer_cont.h
+++ source/clib/src/util/citer_cont.h
@@ -1,6 +1,8 @@
 #ifndef _CITER_CONT_H_
 #define _CITER_CONT_H_
 
+#include <citer_base.h>
+
 extern inline int citer_dis(const citer from, const citer to);
 
 extern inline void citer_move_n(citer itr, int dis);
