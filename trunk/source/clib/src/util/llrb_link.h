1f63a9472f06ef732c4b0cf62920ad2171081284
March1896@gmail.com <March1896@gmail.com@3189ae6c-2c87-11de-a35f-41c58be005ae>
2012-09-18 09:08:03 +0000

    move cntr related code to folder cntr, and add llrb_link and test. This link seems correct and can serve as heap free block container.

---
+++ trunk/source/clib/src/util/llrb_link.h
@@ -0,0 +1,25 @@
+#ifndef _LLRB_LINK_H_
+#define _LLRB_LINK_H_
+
+#include <cominc.h>
+
+/*
+ * LLRB 2-3 tree, a direct copy from Sedgewick's paper
+ */
+struct llrb_link {
+	struct llrb_link *left;
+	struct llrb_link *right;
+	struct llrb_link *parent;
+
+	unsigned     color;
+};
+typedef int (*pf_llrb_compare)(const struct llrb_link *l, const struct llrb_link *r);
+
+struct llrb_link *llrb_insert(struct llrb_link *r, struct llrb_link *n, pf_llrb_compare pc);
+
+struct llrb_link *llrb_remove(struct llrb_link *r, struct llrb_link *n, pf_llrb_compare pc);
+
+void llrb_debug_check(struct llrb_link *root);
+
+#endif /* _LLRB_LINK_H_ */
+
