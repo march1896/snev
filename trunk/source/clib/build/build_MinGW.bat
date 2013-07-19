4a3b8eb0c2322dc8d3f3a1b1b1a6e79d27747cb1
march1896@gmail.com <march1896@gmail.com@3189ae6c-2c87-11de-a35f-41c58be005ae>
2013-07-19 02:55:51 +0000

    Change the build script to support vs2012

--- trunk/source/clib/build/build_MinGW.bat
+++ trunk/source/clib/build/build_MinGW.bat
@@ -1,3 +1,13 @@
 @ECHO OFF
+IF x%MINGW% == "x" GOTO MINGW_NOT_DEFINED
+SET PATH=%PATH%;%MINGW%\bin
+
 build_platform.bat "MinGW Makefiles"
+goto END
+
+:MINGW_NOT_DEFINED
+echo MINGW not defined!
+goto END
+
+:END
 pause
--- trunk/source/clib/build/build_platform.bat
+++ trunk/source/clib/build/build_platform.bat
@@ -1,10 +1,15 @@
 @ECHO OFF
-IF %1=="" GOTO END
+if exist set_env.bat call set_env.bat
+
+IF %1=="" GOTO PLATFORM_NOT_DEFINED
 SET PLATFORM=%1 
-rem here we may pass other parameters
+
+IF x%VCMAKE% == "x" GOTO VCMAKE_NOT_DEFINED
+SET PATH=%PATH%;%VCMAKE%\bin
 
 IF %PLATFORM%=="MinGW Makefiles" SET OUT_DIR=MinGW
 IF %PLATFORM%=="Visual Studio 10" SET OUT_DIR=VS10
+IF %PLATFORM%=="Visual Studio 11" SET OUT_DIR=VS11
 IF %PLATFORM%=="Eclipse CDT4 - MinGW Makefiles" SET OUT_DIR=ECLIPSE
 
 SET OLDDIR=%CD%
@@ -17,5 +22,19 @@ CD %BUILDDIR%%OUT_DIR%
 cmake -G %PLATFORM% ..\..\src
 CD %OLDDIR%
 pause
+goto END
+
+:PLATFORM_NOT_DEFINED
+echo Platform not defined!
+echo must one of:
+echo   MinGW Makefiles
+echo   Visual Studio 10
+echo   Visual Studio 11
+echo   Eclipse CDT4 - MinGW Makefiles
+goto END
+
+:VCMAKE_NOT_DEFINED
+echo VCMAKE not defined!
+goto END
 
 :END
